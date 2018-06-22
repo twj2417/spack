7ae0e0f2bd6b3724faa80f5bdbeb6420da67edec
Satish Balay <balay@mcs.anl.gov>
2018-06-22 10:46:03 +0530

    trilinos@develop: fix build on Mac/clang
    ==> Installing trilinos
    ==> Trying to clone git repository: https://github.com/trilinos/Trilinos.git on branch develop
    ==> No checksum needed when fetching with git
    ==> Already staged trilinos-develop-irg6jlvsjqnidqpfwzjywcxrbtjbx42x in /Users/balay/spack/var/spack/stage/trilinos-develop-irg6jlvsjqnidqpfwzjywcxrbtjbx42x
    1 out of 1 hunk FAILED -- saving rejects to file packages/seacas/cmake/FortranSettings.cmake.rej
    ==> Patch xlf_seacas.patch failed.
    ==> Error: ProcessError: Command exited with status 1:
        '/usr/bin/patch' '-s' '-p' '1' '-i' '/Users/balay/spack/var/spack/repos/builtin/packages/trilinos/xlf_seacas.patch' '-d' '.'
    ==> Error: [Errno 2] No such file or directory: '/Users/balay/spack/var/spack/stage/trilinos-develop-irg6jlvsjqnidqpfwzjywcxrbtjbx42x/Trilinos/spack-build.out'

--- var/spack/repos/builtin/packages/trilinos/package.py
+++ var/spack/repos/builtin/packages/trilinos/package.py
@@ -313,7 +313,7 @@ class Trilinos(CMakePackage):
     patch('xlf_seacas.patch', when='@12.10.1:%xl_r')
     patch('xlf_tpetra.patch', when='@12.12.1:%xl')
     patch('xlf_tpetra.patch', when='@12.12.1:%xl_r')
-    patch('xlf_seacas.patch', when='@12.12.1:%clang')
+    patch('xlf_seacas.patch', when='@12.12.1%clang')
     patch('xlf_tpetra.patch', when='@12.12.1:%clang')
 
     def url_for_version(self, version):
